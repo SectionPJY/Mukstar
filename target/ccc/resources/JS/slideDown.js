b702e7a9a3826581925bd469fb2717c46ea59e39
Marlin-1013 <84124723+Marlin-1013@users.noreply.github.com>
2023-05-20 04:48:03 +0900

    Delete target directory
    필요없음

--- target/ccc/resources/JS/slideDown.js
+++
@@ -1,9 +0,0 @@
-//header slideDown  js 파일
-$(document).ready(function(){
-    $('nav ul li').mouseenter(function(){
-        $(this).children('ul').stop().slideDown();
-    })
-    $('nav ul li').mouseleave(function(){
-        $(this).children('ul').stop().slideUp();
-    })
-})
--- target/ccc/resources/JS/test.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('nav ul li').mouseenter(function(){
-        $(this).children('ul').stop().slideDown();
-    })
-    $('nav ul li').mouseleave(function(){
-        $(this).children('ul').stop().slideUp();
-    })
-})
