392149ae57bc47f7431de290f4724558a860f274
jms <brilliancesik@gmail.com>
2023-10-05 00:02:32 +0900

    Front 관리자 페이지 일부 제작

--- src/main/webapp/resources/JS/admin/demo/chart-bar-demo.js
+++ src/main/webapp/resources/JS/admin/demo/chart-bar-demo.js
@@ -32,13 +32,13 @@ var ctx = document.getElementById("myBarChart");
 var myBarChart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ["January", "February", "March", "April", "May", "June"],
+    labels: ["금", "토", "일", "월", "화", "수", "목", "오늘"],
     datasets: [{
       label: "Revenue",
       backgroundColor: "#4e73df",
       hoverBackgroundColor: "#2e59d9",
       borderColor: "#4e73df",
-      data: [4215, 5312, 6251, 7841, 9821, 14984],
+      data: [4215, 5312, 6251, 7841, 9821, 14984, 9500, 10000],
     }],
   },
   options: {
@@ -61,7 +61,7 @@ var myBarChart = new Chart(ctx, {
           drawBorder: false
         },
         ticks: {
-          maxTicksLimit: 6
+          maxTicksLimit: 8 // 막대 바 아래 요일 개수 보여주는 코드
         },
         maxBarThickness: 25,
       }],
