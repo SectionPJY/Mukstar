4c90a3c6ea5ae5d2eb0e8c48b570c2ad04354d08
SectionPJY <84220538+SectionPJY@users.noreply.github.com>
2023-11-23 09:27:25 +0900

    Add Function.(QnA Update)

--- src/main/java/com/spring/mukstar/dao/QnADAO.java
+++ src/main/java/com/spring/mukstar/dao/QnADAO.java
@@ -5,7 +5,9 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class QnADAO {
@@ -18,6 +20,20 @@ public class QnADAO {
         System.out.println("===== QnA List DAO =====");
 
         return sqlSession.selectList("QnAMapper.qnaList");
+    }
+
+    // QnA Update
+    public int updateQna(int q_id, String q_uid, String q_sub, String q_contents) {
+        System.out.println("===== QnA Update DAO =====");
+        System.out.println(q_id + ", " + q_uid + ", " + q_sub + ", " + q_contents);
+
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("q_id", q_id);
+        map.put("q_uid", q_uid);
+        map.put("q_sub", q_sub);
+        map.put("q_contents", q_id);
+
+        return sqlSession.update("QnAMapper.updateQna", map);
     }
 
     /*
