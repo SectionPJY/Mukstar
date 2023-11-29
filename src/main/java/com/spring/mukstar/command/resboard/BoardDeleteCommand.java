12e7e1efaa42e8b9b2e8c057bab7b4fa8370a50f
marlin-1013 <kjskjs11288@naver.com>
2023-11-29 11:51:04 +0900

    유저 게시물삭제 수정

--- src/main/java/com/spring/mukstar/command/resboard/BoardDeleteCommand.java
+++ src/main/java/com/spring/mukstar/command/resboard/BoardDeleteCommand.java
@@ -14,17 +14,20 @@ public class BoardDeleteCommand {
 
     public int execute(HttpServletRequest request) {
         System.out.println("===== Board Delete Command is Running =====");
+        String data = request.getParameter("rb_id");
+        if (data.isEmpty()){
+            return 0;
+        }else {
+            int rb_id = Integer.parseInt(data);
+            System.out.println("게시글 번호 : " + rb_id);
+            int result = dao.boardDelete(rb_id);
+            if (1 == result) {
+                System.out.println("===== Delete Complete =====");
+            } else {
+                System.out.println("===== Delete Fail =====");
+            }
 
-        int rb_id = Integer.parseInt(request.getParameter("rb_id"));
-        System.out.println("게시글 번호 : " + rb_id);
-
-        int result = dao.boardDelete(rb_id);
-        if (1 == result) {
-            System.out.println("===== Delete Complete =====");
-        } else {
-            System.out.println("===== Delete Fail =====");
+            return result;
         }
-
-        return result;
     }
 }
